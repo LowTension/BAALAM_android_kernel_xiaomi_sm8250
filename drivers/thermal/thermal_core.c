01ae2d5acab7c2a7ea0006e43f27a08e922d3b85
Jeson Gao <jeson.gao@unisoc.com>
2018-09-04 15:14:08 +0800

    thermal: core: using power_efficient_wq for thermal worker
    For SMP systems, thermal worker should use power_efficient_wq in power
    saving mode, that will make scheduler more flexible on selecting an active
    core for running work handler to avoid keeping work handler always
    running on a single core, that will save some power.
    
    Even if 'power_efficient_wq' relevant configs are disabled
    'system_freezable_power_efficient_wq' is identical to system_freezable_wq,
    behavior is unchanged.
    
    Signed-off-by: Jeson Gao <jeson.gao@unisoc.com>
    Signed-off-by: Chunyan Zhang <chunyan.zhang@unisoc.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -328,7 +328,8 @@ static void thermal_zone_device_set_polling(struct workqueue_struct *queue,
 				 round_jiffies(msecs_to_jiffies(delay)));
 	else if (delay)
 		mod_delayed_work(system_freezable_power_efficient_wq,
-				 &tz->poll_queue, msecs_to_jiffies(delay));
+				 &tz->poll_queue,
+				 msecs_to_jiffies(delay));
 	else
 		cancel_delayed_work(&tz->poll_queue);
 }
