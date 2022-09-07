17b3788c352ebce5f59860fa598a3c34cff8f7ff
LowTension <zonarmr@gmail.com>
2022-09-06 20:08:11 -0500

    Revert "soc: qcom: qmi_rmnet: Disable use of alarm timer"
    This reverts commit 8dc99e02a51230b5885c644784b23a329c81e070.

--- drivers/soc/qcom/qmi_rmnet.c
+++ drivers/soc/qcom/qmi_rmnet.c
@@ -1223,7 +1223,7 @@ static void qmi_rmnet_check_stats(struct work_struct *work)
 	u64 rxd, txd;
 	u64 rx, tx;
 	bool dl_msg_active;
-	bool use_alarm_timer = false;
+	bool use_alarm_timer = true;
 
 	real_work = container_of(to_delayed_work(work),
 				 struct rmnet_powersave_work, work);
