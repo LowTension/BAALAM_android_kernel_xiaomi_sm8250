944a56f3275c0d1cb22a329fae5af828475f4935
LowTension <zonarmr@gmail.com>
2022-09-17 00:34:39 -0500

    Revert "disp: msm: dp: support dp for rb5"
    This reverts commit 1d5f7b894446d5e14b9a99267e999df92baf83e7.

--- techpack/display/msm/dp/dp_display.h
+++ techpack/display/msm/dp/dp_display.h
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -71,8 +70,6 @@ struct dp_display {
 	bool is_sst_connected;
 	bool is_mst_supported;
 	u32 max_pclk_khz;
-	u32 max_hdisplay;
-	u32 max_vdisplay;
 	u32 no_mst_encoder;
 	void *dp_mst_prv_info;
 	bool is_primary;
