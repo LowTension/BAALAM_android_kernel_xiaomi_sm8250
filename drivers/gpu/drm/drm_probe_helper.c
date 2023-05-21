7e6d4ea5e72feb8181838dc2559a8b2efb1b418c
LowTension <zonarmr@gmail.com>
2023-05-20 22:37:05 -0600

    drm/probe-helper: use power efficient workingqueues
    Signed-off-by: LowTension <zonarmr@gmail.com>

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -460,8 +460,8 @@ retry:
 		 */
 		dev->mode_config.delayed_event = true;
 		if (dev->mode_config.poll_enabled)
-			schedule_delayed_work(&dev->mode_config.output_poll_work,
-					      0);
+				queue_delayed_work(system_power_efficient_wq, 
+								   &dev->mode_config.output_poll_work, 0);
 	}
 
 	/* Re-enable polling in case the global poll config changed. */
